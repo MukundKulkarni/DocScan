a5f7d0b2b13894c5aabeccb769e9de77152820f2
MukundKulkarni <mukundkulkarni786@gmail.com>
2020-09-30 21:26:53 +0530

    Complete Project

---
+++ app/src/main/java/com/example/docscan/ImageEnhanceActivity.java
@@ -0,0 +1,117 @@
+package com.example.docscan;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.docscan.libraries.NativeClass;
+import com.example.docscan.helpers.MyConstants;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class ImageEnhanceActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Bitmap selectedImageBitmap;
+
+    Button btnImageToBW;
+    Button btnImageToMagicColor;
+    Button btnImageToGray;
+    Button btnSave;
+
+    NativeClass nativeClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_enhance);
+
+        initializeElement();
+        initializeImage();
+    }
+
+    private void initializeElement() {
+
+        nativeClass = new NativeClass();
+
+        imageView = findViewById(R.id.imageView);
+
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveImage(selectedImageBitmap);
+            }
+        });
+
+
+    }
+
+    private void initializeImage() {
+
+        selectedImageBitmap = MyConstants.selectedImageBitmap;
+        MyConstants.selectedImageBitmap = null;
+
+        imageView.setImageBitmap(selectedImageBitmap);
+
+
+    }
+
+
+
+    private void SaveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES).toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            // sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,
+            //     Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+// Tell the media scanner about the new file so that it is
+// immediately available to the user.
+        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+    }
+
+
+}
+
